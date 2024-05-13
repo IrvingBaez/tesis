f83ca70cea04e4b2f9ca51db3ccff65d7583d6cb
Irving Baez <irvingbaezc@gmail.com>
2024-05-12 21:52:45 -0600

    updated test link

--- set_up_env.sh
+++ set_up_env.sh
@@ -1,7 +1,5 @@
 env_name='ava-exp'
 
-source /export/b03/carlosc/miniconda3/etc/profile.d/conda.sh
-
 if conda info --envs | grep -q ${env_name}
 then
   echo "Environment ${env_name} already exists"
