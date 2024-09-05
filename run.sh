bc62cd70c6e15821fb056d500a668072e802f34a
Irving Baez <irvingbaezc@gmail.com>
2024-09-04 19:49:58 -0600

    Fixing Multi-GPU processing

--- run.sh
+++ run.sh
@@ -17,8 +17,8 @@ export CUDA_VISIBLE_DEVICES=0
 export MASTER_ADDR='localhost'
 export MASTER_PORT='12355'
 # export NCCL_P2P_DISABLE='1'
-export NCCL_BLOCKING_WAIT=1
-export NCCL_ASYNC_ERROR_HANDLING=1
+export TORCH_NCCL_BLOCKING_WAIT=1
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 
 program="model/experiments.py"
 if [ "$#" -gt 0 ]; then
