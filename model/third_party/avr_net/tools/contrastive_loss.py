Fixed distance calculation for contrstive loss

model/third_party/avr_net/tools/contrastive_loss.py
@@ -8,16 +8,18 @@ class ContrastiveLoss(nn.Module):
 		self.margin = margin
 
 
-	def forward(self, distance, y):
-		margins = self.margin * torch.ones(distance.shape, device=distance.device)
-		ones = torch.ones(y.shape, device=y.device)
+	def forward(self, scores, y):
+		margins = self.margin * torch.ones(scores.shape, device=scores.device)
+
+		# 1 = similar or 0 distance. 0 = different, or 1 distance.
+		distance = 1 - scores
 
 		# y=1 for similar pairs. y=0 for disimilar pairs
 		# Positive label. All distances add up. Minimizes loss by minimizing distance.
 		pos = y*(1/2)*(distance)**2
 
 		# Negative label. Only distances less than the margin add up. Minimizes loss by maximizing distance.
-		neg = (ones - y)*(1/2)*(torch.clamp(margins - distance, min=0.0))**2
+		neg = (1 - y)*(1/2)*(torch.clamp(margins - distance, min=0.0))**2
 
 		loss = torch.mean(neg + pos)
 